6342110997bb024c15be672860c94338d0b6713d
Md Ataullah <ataullahm100@gmail.com>
2024-03-13 11:32:46 +0600

    Designing

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-11/12 mx-auto">
+        <h1 className="text-5xl text-center font-extrabold">Hello Ataullah</h1>
+        <div className="flex gap-6 justify-between items-start">
+          <div className="w-9/12 grid grid-cols-3 gap-4">
+            <div className="p-4 bg-rose-400 rounded-lg">
+              <div className="w-full h-56 bg-fuchsia-200 rounded-lg">
+                <img src="" alt="" />
+              </div>
+              <div className="mt-6 space-y-2">
+                <h1 className="text-2xl font-medium">Books Name</h1>
+                <p>
+                  Books description Lorem ipsum dolor si Lorem ipsum dolor si
+                  Lorem ipsum dolor sit amet.
+                </p>
+                <p className="text-xl">
+                  Price:{' '}
+                  <span className="font-bold">
+                    <del>$30</del> - $25
+                  </span>
+                </p>
+
+                <button className="bg-green-400 px-5 py-2 rounded-md font-semibold">
+                  Add to card
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="bg-emerald-200 w-1/4 min-h-64 rounded-lg"></div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
